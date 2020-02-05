364425fccca88ea89d34cf9bd28a404d2e63b09c
Daniel H Crane <daniel.h.crane@outlook.com>
2020-02-05 12:17:01 -0500

    Corrected Loop errors and started highlight method

--- ToDoList_Application/Program.cs
+++ ToDoList_Application/Program.cs
@@ -1,79 +1,105 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
+
+
 
 namespace ToDoList_Application
 {
     class Program
     {
+        public static List<string> toDoList = new List<string> { };
         static void Main(string[] args)
         {
+            do
+            {
+                Menu();
 
-            Console.WriteLine("What would you like to do?\n" +
+                string userInput = Console.ReadLine();              //declares that userInput equals what the user types
+
+                if (userInput == "A")                               //will analyze what the user input and call method based on their command (A, S, F, etc.)
+                {
+                    Console.WriteLine("Type New Task:");                //After "A" is input, asks this question. 
+
+                    Add();
+                    
+                    Console.Clear();
+
+                    for (int i = 0; i < toDoList.Count; i++)
+                    {
+                        Highlight();
+
+                        Console.WriteLine(toDoList[i]);
+
+                        StopHightlighting();
+                    }
+                }
+                //else if (userInput == "S")
+                //{
+                //    Skip();
+                //}
+                //else if (userInput == "F")
+                //{
+                //    Finish();
+                //}
+                //else if (userInput == "FL")
+                //{
+                //    FinishLater();
+                //}
+                else                                                //Catch for invalid command
+                {
+                    Console.WriteLine("Command Not Recognized");
+                }   
+            } while (true);
+            //Remove();
+        
+            
+        }
+
+        private static void StopHightlighting()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        private static void Highlight()
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            //Console.WriteLine('\n');
+        }
+
+        private static void Add()
+        {
+            toDoList.Add(Console.ReadLine());
+        }
+
+        private static void Menu()
+        {
+
+            Console.WriteLine("\nWhat would you like to do?\n" +
                                 "A = Add Task, S = Skip, F = Finish / Complete, FL = Finish Later");
-
-                                                                //Very first thing to display. Need userInput
-
-            string userInput = Console.ReadLine();              //declares that userInput equals what the user types
-            List<string> toDoList = new List<string>();
-
-            if (userInput == "A")                               //will analyze what the user input and call method based on their command (A, S, F, etc.)
-            {
-                toDoList = AddString(toDoList);
-            }
-            else if (userInput == "S")
-            {
-                Skip();
-            }
-            else if (userInput == "F")
-            {
-                Finish();
-            }
-            else if (userInput == "FL")
-            {
-                FinishLater();
-            }
-            else                                                //Catch for invalid command
-            {
-                Console.WriteLine("Command Not Recognized. Please Use One Of The Following Commands:\n " +
-                                  "A = Add Task, S = Skip, F = Finish / Complete, FL = Finish Later");
-            }
             
-            Remove();
         }
+        //private static void Remove()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private static void Remove()
-        {
-            throw new NotImplementedException();
-        }
+        //private static void FinishLater()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private static void FinishLater()
-        {
-            throw new NotImplementedException();
-        }
+        //private static void Finish()
+        //{
+        //    throw new NotImplementedException();
+        //}
 
-        private static void Finish()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static void Skip()
-        {
-            throw new NotImplementedException();
-        }
-
-        private static List<string> AddString(List<string> toDoList)
-        {
-            Console.WriteLine("Type New Task:");                //After "A" is input, asks this question. 
-            
-            string userInput = Console.ReadLine();              //declares userInput as whatever the user types in
-                     //declares ToDoList as a "List"
-
-            toDoList.Add(userInput + "\n");                     //declares that ToDoList must add userInput to list and move to new line
-
-            Console.WriteLine(toDoList);                        //display ToDoList
-            return toDoList;
-    
-
-        }
+        //private static void Skip()
+        //{
+        //    throw new NotImplementedException();
+        //}
     }
 }
